58d9ba8759cec23c009695cc59bb3512b053f637
kevinhouguet <k.houguet@gmail.com>
2023-05-13 14:37:35 +0200

    refactor(app): add change to expdate and cvc

--- js/main.js
+++ js/main.js
@@ -14,9 +14,7 @@ const app = {
 
   render: (elementToModify, newValue) => {
     const htmlElementToModify = document.querySelector(`#${elementToModify}`)
-
-    // console.log(childElementToModify);
-    // console.log(newValue)
+    console.log('RENDER', elementToModify, newValue)
 
     if(elementToModify === 'cardNumber') {
       // 12345678 needs to be 1234 5678
@@ -32,13 +30,20 @@ const app = {
 
   createListeners: () => {
     const form = document.querySelector('.card-form');
-    const cardHolderName = form.querySelector('#cardholder-name')
-    const cardNumber = form.querySelector('#card-number')
-    const expdateMM = form.querySelector('#expdate-MM')
-    const expdateYY = form.querySelector('#expdate-YY')
-    const cvc = form.querySelector('#cvc')
-    app.addCustomListener(cardHolderName,'cardHolderName','input')
-    app.addCustomListener(cardNumber,'cardNumber','input')
+    // Create a listener for each input with the same name as the key of the state to itierate on it
+    const input = {
+      cardHolderName: form.querySelector('#cardholder-name'),
+      cardNumber: form.querySelector('#card-number'),
+      expDateMM: form.querySelector('#expdate-MM'),
+      expDateYY: form.querySelector('#expdate-YY'),
+      cvc: form.querySelector('#cvc'),
+    }
+
+    // itierate on the state to create a listener for each key
+    Object.keys(app.state).forEach((key) => {
+      app.addCustomListener(input[key], key, 'input')
+    })
+
     form.addEventListener('submit', app.handleSubmitForm);
   },
 
@@ -53,48 +58,13 @@ const app = {
   handleSubmitForm: (event) => {
     event.preventDefault();
     const formdata = new FormData(event.currentTarget)
-    app.truc = 'truc'
     for (const key of formdata.keys()) {
       app.state[key] = formdata.get(key) === ""
                                       ? app.state[key] : formdata.get(key)
     }
     app.showConfirmLayout();
-    app.changeCard();
   },
-  changeCard: () => {
-    // change the back of card
-    const cardBack = document.querySelector('.card-back')
-    cardBack.querySelector('.card-back__cvc').textContent = app.state.cvc
-
-    // change the front of card
-    const cardFront = document.querySelector('.card-front')
-
-    // change the card number
-    const spanForNumber = cardFront.querySelector('.card-front__number').querySelectorAll('span')
-    const cardNumbersArray = app.splitCardNumber(app.state.cardNumber)
-    spanForNumber.forEach((span, i) => {
-      span.textContent = cardNumbersArray[i]
-    })
-
-    // change the name
-    const cardName = cardFront.querySelector('.card-front_owner')
-    cardName.textContent = app.state.name.toUpperCase()
-
-    // change the exp date
-    const cardExpDate = cardFront.querySelector('.card-front_expiry-date')
-    cardExpDate.textContent = `${app.state.expDateMM}/${app.state.expDateYY}`
-  },
-  splitCardNumber: (cardNumber) => {
-    // We needing an array of string
-    // 12345678 needs to be split in array like that : ['1234', '5678']
-    const result = []
-    const nbRound = 4;
-    for (let i = 0; i < nbRound; i++) {
-      const rangeNb = 4;
-      result.push(cardNumber.substring(0+(rangeNb*i),4+(rangeNb*i)))
-    }
-    return result
-  },
+  
   showConfirmLayout: () => {
     // hide form
     const cardForm = document.querySelector('.card-form')
